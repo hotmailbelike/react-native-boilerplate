4d56cfba74945fb6ef5e44d204520b9f39efd922
Nayem Abs <vlad@pop-os.localdomain>
2020-02-24 14:10:16 +0600

    Specific Search form created

--- src/Search/SpecificSearchView.js
+++ src/Search/SpecificSearchView.js
@@ -11,13 +11,61 @@ import {
   Left,
   Right,
   Body,
-  Icon,
   Text,
+  Item,
+  View,
+  Form,
+  Input,
+  Root,
+  Picker
+
 } from 'native-base';
-import {StyleSheet} from 'react-native';
+import {StatusBar, StyleSheet} from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
 //import { withNavigationFocus } from 'react-navigation'
 
 export default class SpecificSearchView extends React.Component {
+    state = {
+    category: 'Apartment',
+    area:'',
+    short_address: '',
+    description: '',
+    title: '',
+    rate_monthly: '',
+  };
+  onValueChange2(value: string) {
+    this.setState({
+      category: value
+    });
+  }
+  handleSearch = () => {
+    const searchTerm = {searchTerm: this.state.short_address};
+    fetch('https://rentalvr.herokuapp.com/api/rentListings/specificSearch', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(searchTerm),
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            searchResult: result.map(list => ({
+              id: list._id,
+              title: list.title,
+            })),
+          });
+          console.log(this.state.searchResult)
+        },
+        error => {
+          this.setState({isLoaded: false, error});
+        },
+      );
+  };
   // static navigationOptions = {
   //   drawerLabel: 'Search',
   //   drawerIcon: ({tintColor}) => (
@@ -98,42 +146,83 @@ export default class SpecificSearchView extends React.Component {
 
   render() {
     return (
-      <Container>
-        {/* <Header>
-          <Left>
-            <Button transparent>
-              <Icon name="menu" />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right />
-        </Header> */}
-        <Content>
-          <Item>
-            <Text>
-              Here is your chance to be more specific about what you are looking
-              for
-            </Text>
-          </Item>
-          <Item>
-            <Button primary iconRight>
-              Get Started
-            </Button>
-          </Item>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+     
+        <Root>
+        <Container>
+          <Content>
+            <Form>
+              <Item style={{marginBottom: 10}}>
+               <Item picker>
+              <Picker
+                mode="dropdown"
+                iosIcon={<Icon name="arrow-down" />}
+                style={{ width: undefined }}
+                placeholder="Select your category"
+                placeholderStyle={{ color: "#bfc6ea" }}
+                placeholderIconColor="#007aff"
+                selectedValue={this.state.category}
+                onValueChange={this.onValueChange2.bind(this)}                
+              >
+                <Picker.Item label="Apartment" value="apartment" />
+                <Picker.Item label="Office" value="office" />
+                <Picker.Item label="Shop" value="shop" />
+                <Picker.Item label="Garage" value="garage" />
+                <Picker.Item label="Others" value="others" />
+              </Picker>
+            </Item>
+              </Item>
+              <Item style={{marginBottom: 10}}>
+                <Input
+                  placeholder="Area"
+                  onChangeText={text => this.setState({area: text})}
+                />
+              </Item>
+              <Item style={{marginBottom: 10}}>
+                <Input
+                  placeholder="Location"
+                  onChangeText={text => this.setState({short_address: text})}
+                />
+              </Item>
+               <Item style={{marginBottom: 10}}>
+                <Input
+                  placeholder="Monthly Rate"
+                  onChangeText={text => this.setState({rate_monthly: text})}
+                />
+              </Item>
+
+              <View style={this.state.error && styles.signInError}>
+                <Text style={{color: '#ff0000'}}>
+                  {this.state.error && this.state.error}
+                </Text>
+              </View>
+
+              <Button block danger onPress={this.handleSearch}>
+                <Text>Search</Text>
+              </Button>
+
+              {/* <TouchableOpacity
+                    onPress={() => {
+                      this.props.navigation.navigate('UserAccount');
+                           }} >
+                         <Text>Sign IN</Text>
+                    </TouchableOpacity> */}
+              {/* <Button  onPress={() => {
+                      this.props.navigation.navigate('UserAccount');
+                           }} >
+                           <Text>Sign IN</Text>
+                    </Button> */}
+            </Form>
+          </Content>
+        </Container>
+      </Root>
+     
     );
   }
 }
+
+
+
+
 
 SpecificSearchView.navigationOptions = ({navigation}) => ({
   header: (
@@ -145,11 +234,14 @@ SpecificSearchView.navigationOptions = ({navigation}) => ({
         </Button>
       </Left>
       <Body>
-        <Title>Search</Title>
+        <Title>Searchhh</Title>
       </Body>
       <Right />
     </Header>
   ),
 });
+
+
+
 
 const styles = StyleSheet.create({});
